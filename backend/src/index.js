12780e85c5497956dd608c4dac2880dabf12ed7d
Akshat <akshatksingh25@gmail.com>
2025-07-15 12:57:48 +0530

    Update

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,10 +34,7 @@ if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
   app.get("*", (req,res) => {
-    if (req.originalUrl.startsWith("/api")) {
-    return next(); // Let API routes pass through to their routers
-  }
-  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   })
 }
 
